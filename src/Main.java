2fb0acb37647afe4aa179033fdd2182c4571d710
sharayura <sharapyura@gmail.com>
2022-08-25 09:33:32 +0300

    first level

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Author tolstoy = new Author("Лев", "Толстой");
+        Book warPeace = new Book(tolstoy, "Война и мир", 2000);
+        warPeace.setYear(2012);
+        System.out.printf("%s %s '%s', %d", warPeace.getAuthor().getName(), warPeace.getAuthor().getLastname(),
+                warPeace.getTitle(), warPeace.getYear());
+        System.out.println();
+
+        Author chehov = new Author("Антон", "Чехов");
+        Book annaKar = new Book(chehov, "Анна Каренина", 1990);
+        annaKar.setYear(2020);
+        System.out.printf("%s %s '%s', %d", annaKar.getAuthor().getName(), annaKar.getAuthor().getLastname(),
+                annaKar.getTitle(), annaKar.getYear());
+        System.out.println();
+
     }
 }
